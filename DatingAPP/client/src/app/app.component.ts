343daf4fd65818fc11bdda69427f62bf45df4460
arielfriedma <fried.ariel@gmail.com>
2022-03-28 08:37:32 +0300

    section2

---
+++ DatingAPP/client/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { response } from 'express';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Ariel Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe(res=>{
+      this.users=res;
+    },
+    err=>{
+      console.log(err);
+    },
+    ()=>{
+      console.log('Users Loaded');
+    });
+    //   {
+    //   next:(data)=>{
+    //     this.users=data;
+    //   },
+    //   error:(err)=>{
+    //     console.log(err);
+    //   },
+    //   complete:()=>{
+    //     console.log('complete');
+    //   }
+    // })
+  }
+}
